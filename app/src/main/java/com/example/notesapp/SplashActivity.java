9036517536f4b45f4ece75ff4cf91c5f94b566ca
shamshodbek <shamshodbekrahmataliyev@gmail.com>
2024-04-04 15:51:28 +0500

    commit

---
+++ app/src/main/java/com/example/notesapp/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.notesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser==null){
+                    startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                }else{
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                }
+                finish();
+            }
+        },1000);
+
+    }
+}
